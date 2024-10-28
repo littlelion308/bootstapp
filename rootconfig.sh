aa3167de555192323f1dd438e5f865afbdf90916
Little Lion <littlelion308@gmx.de>
2024-10-28 09:41:47 +0100

    removed keybord configuration, as X11 is nolonger in use

--- rootconfig.sh
+++ rootconfig.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
-# _                 _       _                                         _   
-#| |__   ___   ___ | |_ ___| |_ _ __ __ _ _ __        _ __ ___   ___ | |_ 
+# _                 _       _                                         _
+#| |__   ___   ___ | |_ ___| |_ _ __ __ _ _ __        _ __ ___   ___ | |_
 #| '_ \ / _ \ / _ \| __/ __| __| '__/ _` | '_ \ _____| '__/ _ \ / _ \| __|
-#| |_) | (_) | (_) | |_\__ \ |_| | | (_| | |_) |_____| | | (_) | (_) | |_ 
+#| |_) | (_) | (_) | |_\__ \ |_| | | (_| | |_) |_____| | | (_) | (_) | |_
 #|_.__/ \___/ \___/ \__|___/\__|_|  \__,_| .__/      |_|  \___/ \___/ \__|
-#                                        |_|                              
+#                                        |_|
 # install nvim
 pacman -S neovim git
 # conect nvim to vi and nano
 ln -s /bin/nvim /bin/vi
 ln -s /bin/vi /bin/nano
-# setup sudo 
+# setup sudo
 echo visudo
 visudo
 # generate locales
@@ -18,18 +18,10 @@ echo generate locales
 nvim /etc/locale.gen
 # generate locales
 locale-gen
-# set locale 
+# set locale
 echo configure locale
 nvim /etc/locale.conf
-# set hostname 
+# set hostname
 echo set hostname
-nvim /etc/hostname 
+nvim /etc/hostname
 # enable mutikeys
-echo "partial alphanumeric_keys\
-xkb_symbols "intl_nodeadkeys" {\
-	name[Group1]= "U.S. English - International (without plain dead keys)";\
-	include "us(intl)"\
-	key <TLDE> { [     grave,       asciitilde ] };\
-	key <AE06> { [    6, asciicircum,    onequarter,      asciicircum ] };\
-	key <AC11> { [ apostrophe,        quotedbl ] };\
-};" >> /usr/share/X11/xkb/symbols/us
